c51222fa5f8600671cf9fde99b3cd732b66da186
AliciaBenavides <77942770+AliciaBenavides@users.noreply.github.com>
2022-01-31 18:25:20 -0500

    new

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
-{
+
 const x = "Hello Mr.Macias!" ;
 console.log(x) ;
-}
 
-{
+
+
 let a = 'Alicia Benavides' //string variable
 document.getElementById("intro").innerHTML =
 'Name:' + a;
 
-}
 
-{
+
+
 const moduleName = "Module 2"; // Global variable
 document.getElementById("intro").innerHTML = "This assignment is for " + moduleName;
 
-}
 
-{
+
 
 const y = 'block example' ; // Block variable 
 
 document.write = 'This is my' + y ;
-}
-{
+
+
 
 let b = 'block example';
 document.getElementById("intro").innerHTML =
 "This is my " + b;
 
-}
 
 
-{
+
+
 const semester = {Spring:"16 Weeks", Flex :" 8 weeks"}; // Create an object
 
 document.getElementById("intro").innerHTML = "The length of this class is " + semester.Spring;
 
-}
